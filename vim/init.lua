3ca88cd4118092d411950a1a1e9462eb893f53e4
Sagar Tomar <sagartomar97@gmail.com>
2022-09-30 21:42:26 +0530

    Changed keymapping for fuzzy finding files

--- vim/init.lua
+++ vim/init.lua
@@ -62,7 +62,7 @@ require('lualine').setup {
 vim.g.mapleader = " "
 vim.g.fzf_command_prefix = "Fzf"
 
-vim.keymap.set('n', '<leader>f', ':FzfFiles<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>ff', ':FzfFiles<CR>', { noremap = true })
 vim.keymap.set('n', '<leader>fb', ':FzfBuffers<CR>', { noremap = true })
 vim.keymap.set('n', '<leader>fw', ':FzfWindows<CR>', { noremap = true })
 vim.keymap.set('n', '<leader>fl', ':FzfLines<CR>', { noremap = true })
