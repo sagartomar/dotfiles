31e55cbe5362ef15705ebfd71815344137045f1c
Sagar Tomar <sagartomar97@gmail.com>
2023-01-24 12:38:35 +0530

    Fixed filetype icons not appearing in nvim-tree

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -4,7 +4,7 @@ require("packer").startup(function()
     use 'sainnhe/edge'
     use {
         'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+        requires = { 'kyazdani42/nvim-web-devicons' }
     }
     use {
         'nvim-tree/nvim-tree.lua',
