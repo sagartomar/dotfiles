26c02ad858924f8ec72fdfc6d72240848ec80221
Sagar Tomar <sagartomar97@gmail.com>
2023-08-31 15:06:44 +0530

    Added java support in tresitter

--- vim/lua/setup/treesitter.lua
+++ vim/lua/setup/treesitter.lua
@@ -1,5 +1,5 @@
 require 'nvim-treesitter.configs'.setup {
-    ensure_installed = { "c", "lua", "python", "go", "rust", "javascript", "typescript", "tsx" },
+    ensure_installed = { "c", "lua", "python", "go", "rust", "javascript", "typescript", "tsx", "java" },
     highlight = {
         enable = true
     },
